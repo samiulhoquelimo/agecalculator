886f272c589b1932b58fce33e88160b15f96b4fc
samiulhoquelimo <samiul.limo@gmail.com>
2026-01-03 18:42:11 +0600

    - working on improvise

--- app/src/main/java/com/droidturbo/agecalculator/main/HomeAgeModel.kt
+++
@@ -1,7 +0,0 @@
-package com.droidturbo.agecalculator.main
-
-data class HomeAgeModel(
-    val ageYear: Int = 0,
-    val ageMonth: Int = 0,
-    val ageDay: Int = 0
-)
--- app/src/main/java/com/droidturbo/agecalculator/main/HomeInput.kt
+++
@@ -1,7 +0,0 @@
-package com.droidturbo.agecalculator.main
-
-data class HomeInput(
-    val dayOfMonth: String = "",
-    val month: String = "",
-    val year: String = ""
-)
